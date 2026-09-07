c423156963508bf5570ec65fde1d993a996732bc
princeteck <privateyuvraj@gmail.com>
2026-09-08 02:20:19 +0530

    chore: update hyfens to 0.1.10

--- Formula/hyfens.rb
+++ Formula/hyfens.rb
@@ -1,26 +1,26 @@
 class Hyfens < Formula
   desc "Open-source Flutter live-update infrastructure CLI"
   homepage "https://hyfens.com"
-  version "0.1.9"
+  version "0.1.10"
   license "Apache-2.0"
 
   on_macos do
     if Hardware::CPU.arm?
-      url "https://github.com/hyfens-hq/hyfens/releases/download/v0.1.9/hyfens-0.1.9-macos-arm64.tar.gz"
-      sha256 "41037f5897ddaa0688555f6b95f7222b8079cfd20d9508b55edb3f09b78aa9dd"
+      url "https://github.com/hyfens-hq/hyfens/releases/download/v0.1.10/hyfens-0.1.10-macos-arm64.tar.gz"
+      sha256 "be8104c3a32082b34b40c160d2a20640dc9352c8592f97fa18bdb98b6fdc2c3d"
     else
-      url "https://github.com/hyfens-hq/hyfens/releases/download/v0.1.9/hyfens-0.1.9-macos-x64.tar.gz"
-      sha256 "526e8c1bf0973ad7f5483dee936662b2872daa99ad20d87a54ee7698c9476554"
+      url "https://github.com/hyfens-hq/hyfens/releases/download/v0.1.10/hyfens-0.1.10-macos-x64.tar.gz"
+      sha256 "2cbf5fb6256cd656e606c82f08c7f4689734924c0d4b7e18c8f0fac87656cee1"
     end
   end
 
   on_linux do
     if Hardware::CPU.arm?
-      url "https://github.com/hyfens-hq/hyfens/releases/download/v0.1.9/hyfens-0.1.9-linux-arm64.tar.gz"
-      sha256 "8b8046e3a8d73541bd1e702250e7c07930bde73508f9bb152f6f645b72b09b7e"
+      url "https://github.com/hyfens-hq/hyfens/releases/download/v0.1.10/hyfens-0.1.10-linux-arm64.tar.gz"
+      sha256 "19c71dcd0543e71ac87a614e6be7ad4e8a0005c96bb8ff7226d03c8bcaec3845"
     else
-      url "https://github.com/hyfens-hq/hyfens/releases/download/v0.1.9/hyfens-0.1.9-linux-x64.tar.gz"
-      sha256 "987ba58f636332b5f5e2e9f858f0f9428cfec8c5f8d534949b0cf03cc3911b22"
+      url "https://github.com/hyfens-hq/hyfens/releases/download/v0.1.10/hyfens-0.1.10-linux-x64.tar.gz"
+      sha256 "cb0f97ee79968111267ccb0d7ff692989acb42988f3b522896da459322103cc2"
     end
   end
 
