9bcc3420e0ed74c800c551d5101b72225000ebe4
princeteck <privateyuvraj@gmail.com>
2026-09-07 20:58:12 +0530

    chore: update hyfens to 0.1.9
    Signed-off-by: princeteck <privateyuvraj@gmail.com>

--- Formula/hyfens.rb
+++ Formula/hyfens.rb
@@ -1,26 +1,26 @@
 class Hyfens < Formula
   desc "Open-source Flutter live-update infrastructure CLI"
   homepage "https://hyfens.com"
-  version "0.1.8"
+  version "0.1.9"
   license "Apache-2.0"
 
   on_macos do
     if Hardware::CPU.arm?
-      url "https://github.com/hyfens-hq/hyfens/releases/download/v0.1.8/hyfens-0.1.8-macos-arm64.tar.gz"
-      sha256 "20e667a13e81d6fa2e92c466f4ada8544031b59162ba76af4b607b456293e97c"
+      url "https://github.com/hyfens-hq/hyfens/releases/download/v0.1.9/hyfens-0.1.9-macos-arm64.tar.gz"
+      sha256 "41037f5897ddaa0688555f6b95f7222b8079cfd20d9508b55edb3f09b78aa9dd"
     else
-      url "https://github.com/hyfens-hq/hyfens/releases/download/v0.1.8/hyfens-0.1.8-macos-x64.tar.gz"
-      sha256 "7c7901e5f54555f848a71649ab4dc778b12c797d894bdbb2155a25e4d24012b9"
+      url "https://github.com/hyfens-hq/hyfens/releases/download/v0.1.9/hyfens-0.1.9-macos-x64.tar.gz"
+      sha256 "526e8c1bf0973ad7f5483dee936662b2872daa99ad20d87a54ee7698c9476554"
     end
   end
 
   on_linux do
     if Hardware::CPU.arm?
-      url "https://github.com/hyfens-hq/hyfens/releases/download/v0.1.8/hyfens-0.1.8-linux-arm64.tar.gz"
-      sha256 "10cdc89f7b95b180ff19832d36fb65851298037c7abc15cb8891d6476551319e"
+      url "https://github.com/hyfens-hq/hyfens/releases/download/v0.1.9/hyfens-0.1.9-linux-arm64.tar.gz"
+      sha256 "8b8046e3a8d73541bd1e702250e7c07930bde73508f9bb152f6f645b72b09b7e"
     else
-      url "https://github.com/hyfens-hq/hyfens/releases/download/v0.1.8/hyfens-0.1.8-linux-x64.tar.gz"
-      sha256 "20c9d631cf36ec9c58d7b80601ec2b0017bfb832703122f1674e92da4515133a"
+      url "https://github.com/hyfens-hq/hyfens/releases/download/v0.1.9/hyfens-0.1.9-linux-x64.tar.gz"
+      sha256 "987ba58f636332b5f5e2e9f858f0f9428cfec8c5f8d534949b0cf03cc3911b22"
     end
   end
 
